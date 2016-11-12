15807e4fa264655e8ce72088bbe91d0830a0de9f
Diego <diego.garcia@gmail.com>
2016-11-12 18:36:14 -0500

    Add functionality of change language

--- source/it-services/config/MainController.js
+++ source/it-services/config/MainController.js
@@ -4,23 +4,19 @@ class MainController {
         this.$translate = $translate;
         this.$cookieStore = $cookieStore;
         this.localStorageService = localStorageService;
-        this.languages = {english: 'en-us', spanish: 'es-es'};
         this.defaultLang = this.localStorageService.get('lang');
-        this.yesNo = this.localStorageService.get('langStatus') || true;
     }
 
     $onInit() {
         if (!this.defaultLang) {
-            this.defaultLang = this.languages.english;
+            this.defaultLang = 'en-us';
             this.localStorageService.set('lang', this.defaultLang);
         }
         this.$translate.use(this.defaultLang);
-        this.watchLanguageChange()
     }
 
-
     toggleLanguage() {
-        this.defaultLang = this.yesNo ? this.languages.english : this.languages.spanish;//this.defaultLang === this.languages.english ? this.languages.spanish : this.languages.english;
+        this.defaultLang = this.languageSwitch ? 'es-es' : 'en-us';
         this.$translate.use(this.defaultLang);
         this.localStorageService.set('lang', this.defaultLang);
     }
